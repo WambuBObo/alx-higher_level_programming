985e0805bac894b0adc3354701c31c439548fd3d
WambuBObo <wggachora@gmail.com>
2022-06-09 09:10:17 +0000

    Printing lowercase letters

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
-# Print all lowercase alphaet using ACII
 #!/usr/bin/python3
-for i in range(97,123):
-	print(chr(i),end=' ')
-# Displaying all lowercase alphabets with space
-#Ascii value of lower case alphaets 97-123
+for alphabets in range(97, 123):
+    print("{}".format(chr(alphabets)), end="")
