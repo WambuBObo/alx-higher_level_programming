37cce928b55b959e3ee6635036b52903f9685d6f
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-06-09 12:24:15 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letters in range(97, 123):
-	 if (letters != 113 and letters != 101):
-		print("{}".format(chr(letters)), end="")
-
+    if (letters != 113 and letters != 101):
+        print("{}".format(chr(letters)), end="")
+	
